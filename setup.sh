55e58d085f0e6a21e82f7c310c397840efff2aa5
Josh Netterfield <jnetterfield@palantir.com>
2016-01-20 19:19:26 -0500

    Do not automatically install fish.

--- setup.sh
+++ setup.sh
@@ -18,18 +18,21 @@ cp -fr ./bin/* ~/.config/jnetterf/bin
 read -p "Enter a clipboard github url (ALL HISTORY WILL BE REMOVED): " clipboard_url
 ./bin/hsinst.sh $clipboard_url
 
-curl http://fishshell.com/files/2.2.0/fish-2.2.0.tar.gz > /tmp/fish.tar.gz
-cd /tmp
-tar -xf fish.tar.gz || ln -s ./source.fish ./share/completions/..fish || true # Windows symlink sadface.
+test "q$SETUP_FISH" != "q" && (
+    curl http://fishshell.com/files/2.2.0/fish-2.2.0.tar.gz > /tmp/fish.tar.gz;
+    cd /tmp;
+    tar -xf fish.tar.gz;
+    tar -xf fish.tar.gz || ln -s ./source.fish ./share/completions/..fish || true # Windows symlink sadface.
 
-# Setup fish
-cd ./fish-2.2.0
-./configure --prefix=$HOME/.config/jnetterf/
-make
-make install
+    # Setup fish
+    cd ./fish-2.2.0;
+    ./configure --prefix=$HOME/.config/jnetterf/;
+    make;
+    make install;
 
-# Fish config path cannot be changed (crashes when xdg paths are set)
-mv ~/.config/fish ~/.config/fish.dotjnetterf_hold
-ln -s ~/.config/jnetterf/etc/fish ~/.config/fish
+    # Fish config path cannot be changed (crashes when xdg paths are set)
+    mv ~/.config/fish ~/.config/fish.dotjnetterf_hold;
+    ln -s ~/.config/jnetterf/etc/fish ~/.config/fish;
+)
 
 echo DONE
